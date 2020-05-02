12dfc828e11c3e8d7a4495d952d5b1f2da0b7bf0
kimetix99 <qpico99@gmail.com>
2020-05-02 17:51:04 +0200

    added new base.html

--- Vibely/settings.py
+++ Vibely/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'apps.users',
     'apps.main',
+    'adminlte3',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
@@ -63,7 +64,7 @@ ROOT_URLCONF = 'Vibely.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': ['Vibely/templates/']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
