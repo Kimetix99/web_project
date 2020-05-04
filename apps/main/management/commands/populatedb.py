d9fa530e1d822b9617eb20182e322a11343d2641
Sergi Simon Balcells <sergisiba@gmail.com>
2020-05-04 18:04:52 +0200

    Added partially populatedb
    Known bugs: it does not upload any image, and only Establishment
    and users are implemented, use python3 manage.py flush to delete
    entries at database, use python3 manage.py populatedb to create
    entries at database.

--- apps/main/management/commands/populatedb.py
+++ apps/main/management/commands/populatedb.py
@@ -1,7 +1,8 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from apps.main.models import *
 
+User = get_user_model()
 
 class Command(BaseCommand):
     help = 'Populates the database for testing locally purposes'
@@ -10,12 +11,21 @@ class Command(BaseCommand):
         self._create_users()
         self._create_bands()
         self._create_establishment()
+        self._create_events()
 
     def _create_users(self):
         """
         Creates all users for testing
         """
-        pass
+        self.users = []
+        userparms = [('user1', 'testpass123'),
+                ('user2', 'testpass123'),
+                ('user3', 'testpass123'),
+                ('user4', 'testpass123')]
+        for us, ps in userparms:
+            self.users.append(
+                User.objects.create_user(username=us, password=ps)
+                )
 
     def _create_bands(self):
         """
@@ -27,7 +37,17 @@ class Command(BaseCommand):
         """
         Creates all the establishments.
         """
-        pass
+        self.establishments = [
+            Establishment(name="Bar Pac1", address="C/Major n9", email="paco96@gmail.com", mobile="000000000", user=self.users[0]), 
+            Establishment(name="Bar Pac2", address="C/Major n9", email="paco96@gmail.com", mobile="100000000", user=self.users[1]),
+            Establishment(name="Bar Pac3", address="C/Major n9", email="paco96@gmail.com", mobile="200000000", user=self.users[2]),
+            Establishment(name="Bar Pac4", address="C/Major n9", email="paco96@gmail.com", mobile="300000000", user=self.users[3]),
+        ]
+        for est in self.establishments:
+            est.save()
+
+
+        
 
     def _create_events(self):
         """
