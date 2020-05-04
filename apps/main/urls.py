4337adbfd9f72a450ad596fff1b0a5f986bc1e5a
Sergi Simon Balcells <sergisiba@gmail.com>
2020-05-04 19:34:14 +0200

    Added functionality to list_bands
    Cannot check as it was not updated the populatedb command.

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('band/', views.ListBand.as_view(), name='band_list'),
     path('band/create', views.CreateBandView.as_view(), 
         name='band_create'),
     path('band/<int:pk>', views.BandDetail.as_view(),
         name='band_detail'),
     path('establishment/', views.ListEstablishment.as_view(),
-        name='establishment_list')
+        name='establishment_list'),
 ]
