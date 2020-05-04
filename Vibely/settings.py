109172364021d264914c32e8cea4b25a46b682ed
Sergi Simon Balcells <sergisiba@gmail.com>
2020-05-04 18:04:22 +0200

    Added media url media storage and static media to urls

--- Vibely/settings.py
+++ Vibely/settings.py
@@ -140,6 +140,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
