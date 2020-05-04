1c59e833032e10efc4b5e09a6c8a83712a3bcbe7
Sergi Simon Balcells <sergisiba@gmail.com>
2020-05-04 19:33:52 +0200

    Added tests to list_bands feature

---
+++ apps/main/tests/test_list_bands.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from apps.main.models import Band
+from django.urls import reverse
+
+User = get_user_model()
+
+
+class ListEstablishmentTest(TestCase):
+
+    def setUp(self):
+        user1 = User.objects.create_user(username="paquito", password="testingpaco123", email="paco@gmail.com", first_name="Paco", last_name="Rodriguez")
+        user2 = User.objects.create_user(username="paquito1", password="testingpaco123", email="paco@gmail.co", first_name="Paco", last_name="Rodriguez")
+        user3 = User.objects.create_user(username="paquito2", password="testingpaco123", email="paco@gmail.om", first_name="Paco", last_name="Rodriguez")
+        user4 = User.objects.create_user(username="paquito3", password="testingpaco123", email="paco@gmail.cm", first_name="Paco", last_name="Rodriguez")
+        Band.objects.create(web_link='https://soundcloud.com/skeewiff', playlist='https://soundcloud.com/dj-elye/sets/jazz',      email="skeewiff@gmail.com", mobile="000000000", user=user1)
+        Band.objects.create(web_link='https://soundcloud.com/tessatioarina', playlist='https://soundcloud.com/dj-elye/sets/jazz', email="tessatioarina@gmail.com", mobile="100000000", user=user2)
+        Band.objects.create(web_link='https://soundcloud.com/drei-jazz', playlist='https://soundcloud.com/dj-elye/sets/jazz',     email="tessatioarina@gmail.com", mobile="200000000", user=user3)
+        Band.objects.create(web_link='https://soundcloud.com/angelo-mikha', playlist='https://soundcloud.com/dj-elye/sets/jazz',  email="angelo@gmail.com", mobile="300000000", user=user4)
+
+
+    def test_establishment_list(self):
+        response = self.client.get(reverse('band_list'))
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(response, "skeewiff@gmail.com")
+        self.assertContains(response, "tessatioarina@gmail.com")
+        self.assertContains(response, "angelo@gmail.com")
+        self.assertContains(response, "tessatioarina@gmail.com")
+        self.assertTemplateUsed(response, 'band/list.html)')
+
