be00f51dfb1fb202daa060d56a368fe54f552e56
Navy22junda <navy22junda@gmail.com>
2020-05-07 20:22:11 +0200

    Done create_establishment.py TODO: Frontend

--- features/steps/create_establishment.py
+++ features/steps/create_establishment.py
@@ -2,22 +2,43 @@ from behave import *
 
 use_step_matcher("re")
 
-@when(u'I create a establishment')
+@given(u'Exists a user "user" with password "password"')
+def step_impl(context, user, password):
+    from django.contrib.auth import get_user_model
+    get_user_model().objects.create_user(username=user, password=password, email='b@b.c')
+
+
+@given(u'I login as user "user" with password "password"')
 def step_impl(context):
-    for row in context.table:
-        context.browser.visit(context.get_url(''))
-        if context.browser.url == context.get_url(''):
-            form = context.browser.find_by_tag('').first
-            for heading in row.headings:
-                context.browser.fill(heading, row[heading])
-            form.find_by_value('submit').first.click()
+    context.browser.visit(context.get_url('/accounts/login/?next=/establishment/create/)'))
+    context.browser.fill('username', "user")
+    context.browser.fill('password', "password")
+    context.browser.find_by_name('loginsubmit').first.click()
+
+@when(u'I create a establishment')
+def step_impl(context, name, address, email, mobile):
+    context.browser.visit(
+        context.get_url('establishment_create')
+    )
+    assert context.browser.url == context.get_url('establishment_create')
+    context.browser.fill('name', name)
+    context.browser.fill('address', address)
+    context.browser.fill('email', email)
+    context.browser.fill('mobile', mobile)
+    context.browser.find_by_name('establishmentsubmint').first.click()
 
 
 @then(u'I\'m viewing the details page for establishment by "user"')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I\'m viewing the details page for establishment by "user"')
+def step_impl(context, name, address, email, mobile):
+    from apps.main.models import Establishment
+    establishment = Establishment.objects.filter(
+        name=name,
+        address=address,
+        email=email,
+        mobile=mobile).get()
+    assert context.browser.url == context.get_url(establishment)
 
 
 @then(u'There are 1 establishment')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then There are 1 establishment')
+    assert context.browser.url == context.get_url('/accounts/login/?next=/establishment/create')
